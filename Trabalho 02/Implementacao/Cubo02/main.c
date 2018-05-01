b072184cfa52ec9479aca59316877587ec21e31e
Tiago Loriato <tloriatosimoes@gmail.com>
2018-05-01 16:24:33 -0300

    formating code

--- Trabalho 02/Implementacao/Cubo02/main.c
+++ Trabalho 02/Implementacao/Cubo02/main.c
@@ -3,8 +3,8 @@
 #endif
 
 #include "CUBO.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define ALG 55
@@ -29,88 +29,87 @@
 //		0 -> sucesso
 int populaCubo(int *config) {
 
-	// 1: Verde | 2: Vermelho | 3: Amarelo | 4: Azul | 5: Branco | 6: Laranja
+  // 1: Verde | 2: Vermelho | 3: Amarelo | 4: Azul | 5: Branco | 6: Laranja
 
-	// Up Face 0
+  // Up Face 0
 
-	config[0][0][0] =LARANJA;
-	config[0][0][1]=LARANJA;
-	config[0][0][2] =LARANJA;
+  config[0][0][0] = LARANJA;
+  config[0][0][1] = LARANJA;
+  config[0][0][2] = LARANJA;
 
-	config[0][1][0] =LARANJA;
-	config[0][1][1]=LARANJA;
-	config[0][1][2] =LARANJA;
+  config[0][1][0] = LARANJA;
+  config[0][1][1] = LARANJA;
+  config[0][1][2] = LARANJA;
 
-	config[0][2][0] =LARANJA;
-	config[0][2][1]=LARANJA;
-	config[0][2][2] =LARANJA;
-	// Left Face 4
-	config[4][0][0] = AMARELO;
-	config[4][0][1] = AMARELO;
-	config[4][0][2] = AMARELO;
+  config[0][2][0] = LARANJA;
+  config[0][2][1] = LARANJA;
+  config[0][2][2] = LARANJA;
+  // Left Face 4
+  config[4][0][0] = AMARELO;
+  config[4][0][1] = AMARELO;
+  config[4][0][2] = AMARELO;
 
-	config[4][1][0] = AMARELO;
-	config[4][1][1] = AMARELO;
-	config[4][1][2] = AMARELO;
+  config[4][1][0] = AMARELO;
+  config[4][1][1] = AMARELO;
+  config[4][1][2] = AMARELO;
 
-	config[4][2][0] = AMARELO;
-	config[4][2][1] = AMARELO;
-	config[4][2][2] = AMARELO;
+  config[4][2][0] = AMARELO;
+  config[4][2][1] = AMARELO;
+  config[4][2][2] = AMARELO;
 
-	// Front Face 1
-	config[1][0][0] = AMARELO;
-	config[1][0][1] = AMARELO;
-	config[1][0][2] = AMARELO;
+  // Front Face 1
+  config[1][0][0] = AMARELO;
+  config[1][0][1] = AMARELO;
+  config[1][0][2] = AMARELO;
 
-	config[1][1][0] = AMARELO;
-	config[1][1][1] = AMARELO;
-	config[1][1][2] = AMARELO;
+  config[1][1][0] = AMARELO;
+  config[1][1][1] = AMARELO;
+  config[1][1][2] = AMARELO;
 
-	config[1][2][0] = AMARELO;
-	config[1][2][1] = AMARELO;
-	config[1][2][2] = AMARELO;
+  config[1][2][0] = AMARELO;
+  config[1][2][1] = AMARELO;
+  config[1][2][2] = AMARELO;
 
-	// Right Face 2
-	config[2][0][0] = AMARELO;
-	config[2][0][1] = AMARELO;
-	config[2][0][2] = AMARELO;
+  // Right Face 2
+  config[2][0][0] = AMARELO;
+  config[2][0][1] = AMARELO;
+  config[2][0][2] = AMARELO;
 
-	config[2][1][0] = AMARELO;
-	config[2][1][1] = AMARELO;
-	config[2][1][2] = AMARELO;
+  config[2][1][0] = AMARELO;
+  config[2][1][1] = AMARELO;
+  config[2][1][2] = AMARELO;
 
-	config[2][2][0] = AMARELO;
-	config[2][2][1] = AMARELO;
-	config[2][2][2] = AMARELO;
+  config[2][2][0] = AMARELO;
+  config[2][2][1] = AMARELO;
+  config[2][2][2] = AMARELO;
 
-	// Back Face 3
-	config[3][0][0] = AMARELO;
-	config[3][0][1] = AMARELO;
-	config[3][0][2] = AMARELO;
+  // Back Face 3
+  config[3][0][0] = AMARELO;
+  config[3][0][1] = AMARELO;
+  config[3][0][2] = AMARELO;
 
-	config[3][1][0] = AMARELO;
-	config[3][1][1] = AMARELO;
-	config[3][1][2] = AMARELO;
+  config[3][1][0] = AMARELO;
+  config[3][1][1] = AMARELO;
+  config[3][1][2] = AMARELO;
 
-	config[3][2][0] = AMARELO;
-	config[3][2][1] = AMARELO;
-	config[3][2][2] = AMARELO;
+  config[3][2][0] = AMARELO;
+  config[3][2][1] = AMARELO;
+  config[3][2][2] = AMARELO;
 
-	// Down Face 5
-	config[5][0][0] = BRANCO;
-	config[5][0][1] = BRANCO;
-	config[5][0][2] = BRANCO;
+  // Down Face 5
+  config[5][0][0] = BRANCO;
+  config[5][0][1] = BRANCO;
+  config[5][0][2] = BRANCO;
 
-	config[5][1][0] = BRANCO;
-	config[5][1][1] = BRANCO;
-	config[5][1][2] = BRANCO;
+  config[5][1][0] = BRANCO;
+  config[5][1][1] = BRANCO;
+  config[5][1][2] = BRANCO;
 
-	config[5][2][0] = BRANCO;
-	config[5][2][1] = BRANCO;
-	config[5][2][2] = BRANCO;
+  config[5][2][0] = BRANCO;
+  config[5][2][1] = BRANCO;
+  config[5][2][2] = BRANCO;
 
-	return 0;
-
+  return 0;
 }
 
 // (cubo, algoritmo) ~> inteiro
@@ -120,95 +119,82 @@ int populaCubo(int *config) {
 //	inteiro =
 //		0 -> sucesso
 //		-1 -> entradas invalidas (TODO)
-int executaAlgoritmo(CUB_tpCubo* cubo, char* algoritmo)
-{
+int executaAlgoritmo(CUB_tpCubo *cubo, char *algoritmo) {
 
-	int i = 0;
-	int ret;
+  int i = 0;
+  int ret;
 
-	char comandos[ALG];
-	memset(comandos, '\0', sizeof(comandos));
-	strcpy(comandos, algoritmo);
+  char comandos[ALG];
+  memset(comandos, '\0', sizeof(comandos));
+  strcpy(comandos, algoritmo);
 
-	while (comandos[i]) {
+  while (comandos[i]) {
 
-		if( comandos[i] == 'U'){
-			if ( comandos[i+1] == '\''){
-				CUB_giraTrasDireita(cubo,1);
-				//excuta sentido antihorario
-				i+=3;
-			}
-			else{
-				CUB_giraTopoEsquerda(cubo,1);
-			    //executa sentido horario
-				i+=2;
-			}
-		}
+    if (comandos[i] == 'U') {
+      if (comandos[i + 1] == '\'') {
+        CUB_giraTrasDireita(cubo, 1);
+        // excuta sentido antihorario
+        i += 3;
+      } else {
+        CUB_giraTopoEsquerda(cubo, 1);
+        // executa sentido horario
+        i += 2;
+      }
+    }
 
-		else if( comandos[i] == 'B'){
-			if ( comandos[i+1] == '\''){
-				CUB_giraTopoDireita(cubo,1);
-				//excuta sentido antihorario
-				i+=3;
-			}
-			else {
-				CUB_giraTrasEsquerda(cubo,1);
-			    //executa sentido horario
-				i+=2;
-			}
-		}
+    else if (comandos[i] == 'B') {
+      if (comandos[i + 1] == '\'') {
+        CUB_giraTopoDireita(cubo, 1);
+        // excuta sentido antihorario
+        i += 3;
+      } else {
+        CUB_giraTrasEsquerda(cubo, 1);
+        // executa sentido horario
+        i += 2;
+      }
+    }
 
-			else if( comandos[i] == 'D'){
-			if ( comandos[i+1] == '\''){
-				CUB_giraBaixoDireita(cubo,1);
-				//excuta sentido antihorario
-				i+=3;
-			}
-			else {
-				CUB_giraBaixoEsquerda(cubo,1);
-			    //executa sentido horario
-				i+=2;
-			}
-		}
-		else if( comandos[i] == 'F'){
-			if ( comandos[i+1] == '\''){
-				CUB_giraFrenteDireita(cubo,1);
-				//excuta sentido antihorario
-				i+=3;
-			}
-			else 
-				CUB_giraFrenteEsquerda(cubo, 1);
-			    //executa sentido horario
-				i+=2;
-		}
-		else if( comandos[i] == 'R'){
-			if ( comandos[i+1] == '\''){
-				CUB_giraDireitaFrente(cubo,1);
-				//excuta sentido antihorario
-				i+=3;
-			}
-			else {
-				CUB_giraDireitaTras(cubo, 1);
-			    //executa sentido horario
-				i+=2;
-			}
-		}
-		else if( comandos[i] == 'L'){
-			if ( comandos[i+1] == '\''){
-				CUB_giraEsquerdaTras(cubo,1);
-				//excuta sentido antihorario
-				i+=3;
-			}
-			else{ 
-				CUB_giraEsquerdaFrente(cubo,1);
-			    //executa sentido horario
-				i+=2;
-			}
-		}
+    else if (comandos[i] == 'D') {
+      if (comandos[i + 1] == '\'') {
+        CUB_giraBaixoDireita(cubo, 1);
+        // excuta sentido antihorario
+        i += 3;
+      } else {
+        CUB_giraBaixoEsquerda(cubo, 1);
+        // executa sentido horario
+        i += 2;
+      }
+    } else if (comandos[i] == 'F') {
+      if (comandos[i + 1] == '\'') {
+        CUB_giraFrenteDireita(cubo, 1);
+        // excuta sentido antihorario
+        i += 3;
+      } else
+        CUB_giraFrenteEsquerda(cubo, 1);
+      // executa sentido horario
+      i += 2;
+    } else if (comandos[i] == 'R') {
+      if (comandos[i + 1] == '\'') {
+        CUB_giraDireitaFrente(cubo, 1);
+        // excuta sentido antihorario
+        i += 3;
+      } else {
+        CUB_giraDireitaTras(cubo, 1);
+        // executa sentido horario
+        i += 2;
+      }
+    } else if (comandos[i] == 'L') {
+      if (comandos[i + 1] == '\'') {
+        CUB_giraEsquerdaTras(cubo, 1);
+        // excuta sentido antihorario
+        i += 3;
+      } else {
+        CUB_giraEsquerdaFrente(cubo, 1);
+        // executa sentido horario
+        i += 2;
+      }
+    }
+  }
 
-		
-		
-	}
-
-	return 0;
+  return 0;
 }
